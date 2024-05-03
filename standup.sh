e6b7e4b9a425040fd9444845e8f598fee79fbc7d
SYNically-ACKward <ryan@thenetworkguy.org>
2024-05-03 09:31:21 -0400

    reorganize functions and comments

--- standup.sh
+++ standup.sh
@@ -5,12 +5,80 @@ PFSENSE_INITIAL_PASS="pfsense"
 red=$(tput setaf 1)
 reset=$(tput sgr0)
 
+# Helper functions
+
 function send_keystrokes() {
     for i in "$@"; do
         pvesh set /nodes/$NODE_NAME/qemu/$VMID/sendkey --key "$i"
         sleep 0.1
     done
 }
+
+function cleanup {
+    echo -e "${red}Cleaning up packages before exit...${reset}"
+    apt-get remove -y expect sshpass
+    echo -e "${red}Cleaning up files before exit...${reset}"
+    file_list=("standup.sh" "config.xml" "login.exp" "container_1.sh" "container_2.sh" "container_3.sh")
+    for file in "${file_list[@]}"; do
+        if test -f "/root/$file"; then
+            rm "/root/$file"
+        fi
+    done
+}
+
+# End Helper Functions
+
+function configure_sdn {
+    local default_lan_1="10.1.0.0/24"
+    local default_lan_2="10.2.0.0/24"
+    local default_lan_3="10.3.0.0/24"
+
+    echo "Enter the subnet (in CIDR format) for LAN 1 (default: $default_lan_1):"
+    read lan_1
+    lan_1=${lan_1:-$default_lan_1}
+
+    echo "Enter the subnet (in CIDR format) for LAN 2 (default: $default_lan_2):"
+    read lan_2
+    lan_2=${lan_2:-$default_lan_2}
+
+    echo "Enter the subnet (in CIDR format) for LAN 3 (default: $default_lan_3):"
+    read lan_3
+    lan_3=${lan_3:-$default_lan_3}
+
+    echo -e "${red}Creating SDN Zone 'lan' and installing dnsmasq...${reset}"
+
+    # Update APT repos, install DNSMASQ and disable default DNSMASQ instance
+    apt-get update -qq && apt-get install -y -qq dnsmasq && systemctl disable --now dnsmasq
+
+    # Create SDN Zone
+    pvesh create /cluster/sdn/zones -zone lan -type simple -nodes $NODE_NAME -ipam pve
+
+    echo -e "${red}Created SDN Zone 'lan' and installed dnsmasq successfully...${reset}"
+    echo -e "${red}Creating VNETs 1-3 in new SDN Zone${reset}"
+
+    # Loop to create VNETs 1-3 in SDN Zone 'lan'
+    for (( i=1; i<=3; i++ )); do
+        pvesh create /cluster/sdn/vnets --vnet vnet${i} --zone lan
+    done
+
+    echo -e "${red}Successfully created VNETs 1-3 in lan zone...${reset}"
+    echo -e "${red}Creating subnets and associating with VNETs...${reset}"
+
+    for (( i=1; i<=3; i++ )); do
+        lan_var="lan_$i"
+        subnet=${!lan_var}  # This uses indirect expansion to get the value of the variable named in lan_var
+
+        pvesh create /cluster/sdn/vnets/vnet$i/subnets --subnet $subnet --type subnet
+
+    done
+
+    echo -e "${red}Successfully created subnets, applying networking changes...${reset}"
+
+    pvesh set /cluster/sdn
+
+    echo -e "${red}Successfully applied networking changes...${reset}"
+}
+
 function deploy_pfSense {
     # Set default values
     local ISO_PATH="/var/lib/vz/template/iso/pfesense.iso"
@@ -79,57 +147,6 @@ function deploy_pfSense {
 
 }
 
-function configure_sdn {
-    local default_lan_1="10.1.0.0/24"
-    local default_lan_2="10.2.0.0/24"
-    local default_lan_3="10.3.0.0/24"
-
-    echo "Enter the subnet (in CIDR format) for LAN 1 (default: $default_lan_1):"
-    read lan_1
-    lan_1=${lan_1:-$default_lan_1}
-
-    echo "Enter the subnet (in CIDR format) for LAN 2 (default: $default_lan_2):"
-    read lan_2
-    lan_2=${lan_2:-$default_lan_2}
-
-    echo "Enter the subnet (in CIDR format) for LAN 3 (default: $default_lan_3):"
-    read lan_3
-    lan_3=${lan_3:-$default_lan_3}
-
-    echo -e "${red}Creating SDN Zone 'lan' and installing dnsmasq...${reset}"
-
-    # Update APT repos, install DNSMASQ and disable default DNSMASQ instance
-    apt-get update -qq && apt-get install -y -qq dnsmasq && systemctl disable --now dnsmasq
-
-    # Create SDN Zone
-    pvesh create /cluster/sdn/zones -zone lan -type simple -nodes $NODE_NAME -ipam pve
-
-    echo -e "${red}Created SDN Zone 'lan' and installed dnsmasq successfully...${reset}"
-    echo -e "${red}Creating VNETs 1-3 in new SDN Zone${reset}"
-
-    # Loop to create VNETs 1-3 in SDN Zone 'lan'
-    for (( i=1; i<=3; i++ )); do
-        pvesh create /cluster/sdn/vnets --vnet vnet${i} --zone lan
-    done
-
-    echo -e "${red}Successfully created VNETs 1-3 in lan zone...${reset}"
-    echo -e "${red}Creating subnets and associating with VNETs...${reset}"
-
-    for (( i=1; i<=3; i++ )); do
-	    lan_var="lan_$i"
-	    subnet=${!lan_var}  # This uses indirect expansion to get the value of the variable named in lan_var
-
-	    pvesh create /cluster/sdn/vnets/vnet$i/subnets --subnet $subnet --type subnet
-
-    done
-
-    echo -e "${red}Successfully created subnets, applying networking changes...${reset}"
-
-    pvesh set /cluster/sdn
-
-    echo -e "${red}Successfully applied networking changes...${reset}"
-}
-
 function configure_pfsense {
     local config_file_link="https://raw.githubusercontent.com/SYNically-ACKward/se-lab-automation/main/config.xml"
     local login_script_link="https://raw.githubusercontent.com/SYNically-ACKward/se-lab-automation/main/login.exp"
@@ -194,64 +211,6 @@ function configure_pfsense {
     expect /root/login.exp "$pfsense_ip" "admin" "$PFSENSE_INITIAL_PASS"
 
     echo -e "${red}PfSense configuration complete - firewall now rebooting...${reset}"
-}
-
-function create_servers {
-    echo -e "${red}Updating CT Template Repositories...${reset}"
-    pveam update
-
-    echo -e "${red}Downloading Ubuntu 22.04 template...${reset}"
-
-    template_name=$(pveam available --section system | grep "ubuntu-22.04-standard_22.04-" | head -n 1 | awk '{print $2}')
-
-
-    if [ -z "$template_name" ]; then
-        echo "No matching Ubuntu 22.04 template found."
-    else
-        echo -e "${red}Downloading template: $template_name ${reset}"
-        pveam download local "$template_name"
-    fi
-
-    echo -e "${red}Ensuring SSH keys exist on ProxMox VE Host... Creating if not...${reset}"
-
-    if ! test -f /root/.ssh/id_rsa.pub; then
-        ssh-keygen -b 2048 -t rsa -f /root/.ssh/ -q -N ""
-        echo -e "${red}Creating SSH keys..."
-    else
-        echo -e "${red}SSH Keys already exist. Continuing..."
-    fi
-
-    echo -e "${red}Creating Ubuntu 22.04 containers...${reset}"
-
-    pvesh create /nodes/$NODE_NAME/lxc \
-        --vmid 800 \
-        --ostemplate "local:vztmpl/${template_name}" \
-        --hostname ubuntu1 \
-        --net0 "name=eth0,bridge=vnet2,ip=dhcp,firewall=0" \
-        --ostype "ubuntu" \
-        --password "zscaler" \
-        --storage local-lvm \
-        --ssh-public-keys "$(cat /root/.ssh/id_rsa.pub)" \
-        --nameserver "10.2.0.1" \
-        --start 1
-
-    wget https://raw.githubusercontent.com/SYNically-ACKward/se-lab-automation/main/container_1.sh
-
-    pct push 800 /root/container_1.sh /root/container_1.sh
-
-    pct exec 800 -- /bin/bash /root/container_1.sh
-}
-
-function cleanup {
-    echo -e "${red}Cleaning up packages before exit...${reset}"
-    apt-get remove -y expect sshpass
-    echo -e "${red}Cleaning up files before exit...${reset}"
-    file_list=("standup.sh" "config.xml" "login.exp" "container_1.sh" "container_2.sh" "container_3.sh")
-    for file in "${file_list[@]}"; do
-        if test -f "/root/$file"; then
-            rm "/root/$file"
-        fi
-    done
 }
 
 function deploy_opnsense {
@@ -388,9 +347,52 @@ function deploy_opnsense {
     send_keystrokes "1" "1" "ret"
     sleep 10
     echo -e "${red}Configuration is now complete...${reset}"
+}
+
+function create_servers {
+    echo -e "${red}Updating CT Template Repositories...${reset}"
+    pveam update
+
+    echo -e "${red}Downloading Ubuntu 22.04 template...${reset}"
+
+    template_name=$(pveam available --section system | grep "ubuntu-22.04-standard_22.04-" | head -n 1 | awk '{print $2}')
 
 
+    if [ -z "$template_name" ]; then
+        echo "No matching Ubuntu 22.04 template found."
+    else
+        echo -e "${red}Downloading template: $template_name ${reset}"
+        pveam download local "$template_name"
+    fi
 
+    echo -e "${red}Ensuring SSH keys exist on ProxMox VE Host... Creating if not...${reset}"
+
+    if ! test -f /root/.ssh/id_rsa.pub; then
+        ssh-keygen -b 2048 -t rsa -f /root/.ssh/ -q -N ""
+        echo -e "${red}Creating SSH keys..."
+    else
+        echo -e "${red}SSH Keys already exist. Continuing..."
+    fi
+
+    echo -e "${red}Creating Ubuntu 22.04 containers...${reset}"
+
+    pvesh create /nodes/$NODE_NAME/lxc \
+        --vmid 800 \
+        --ostemplate "local:vztmpl/${template_name}" \
+        --hostname ubuntu1 \
+        --net0 "name=eth0,bridge=vnet2,ip=dhcp,firewall=0" \
+        --ostype "ubuntu" \
+        --password "zscaler" \
+        --storage local-lvm \
+        --ssh-public-keys "$(cat /root/.ssh/id_rsa.pub)" \
+        --nameserver "10.2.0.1" \
+        --start 1
+
+    wget https://raw.githubusercontent.com/SYNically-ACKward/se-lab-automation/main/container_1.sh
+
+    pct push 800 /root/container_1.sh /root/container_1.sh
+
+    pct exec 800 -- /bin/bash /root/container_1.sh
 }
 
 function main_menu {
@@ -401,8 +403,8 @@ function main_menu {
         echo "3) Apply pfSense Base Config"
         echo "4) Deploy and configure OPNsense"
         echo "5) Create 'Server' Containers"
-        # echo "5) Create 'User' VMs"
-        # echo "6) Create 'IOT' Containers"
+        # echo "6) Create 'User' VMs"
+        # echo "7) Create 'IOT' Containers"
         
         echo "q) Quit"
         read -p "Enter your choice: " choice
